94263bcc64fed5fd1613ea94383c39b26697c7e1
曾文建 <wen@cengwenjiande-MacBook-Pro.local>
2019-12-31 17:09:47 +0800

    params test

--- app/api/resturant/v1/api/member_api.rb
+++ app/api/resturant/v1/api/member_api.rb
@@ -2,16 +2,16 @@ module Resturant::V1::Api::MemberApi
     class APIv1 < Grape::API
       resource :members do
         group do
-            params do
-                optional :Zi, type: Hash do
-                    requires :access_token, type: String
-                    requires :expires_at, type: String
-                end
-            end
-            post '/google_call_back' do
+            # params do
+            #     optional :zi, type: Hash do
+            #         requires :access_token, type: String
+            #         requires :expires_at, type: String
+            #     end
+            # end
+            post '/request_token' do
                 puts params
             end
         end
       end
     end
-  end
+end
